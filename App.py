602ac3212667f3416b9b7399d61f14401a868767
emmanuelmitch <dykemitchell5@gmail.com>
2024-11-28 10:00:12 +0100

    initial commit

---
+++ App.py
@@ -0,0 +1,109 @@
+# myName = "Karimat"
+# myAge = "15"
+# myMessage = "I am %s by name and i am %s years old."
+# print(myMessage % (myName, myAge))
+
+# print(type ('20.6'))
+
+# myName = "Karimat"
+# myAge = 15 + 2 
+# myMessage = f"I am {myName} by name and i am {myAge} years old."
+# print(myMessage)
+
+# myName = "Karimat"
+# myMessage = f"I am {myName} by name and i am {5 * 6} years old."
+# print(myMessage)
+
+# text = "Welcome to Softlinks"
+# print(len(text))
+# print(text.upper())
+# print(text.lower())
+# print(text.index("o"))
+# print(text.replace("o", "a"))
+# print(text.count("o"))
+
+# x = str(145)
+# print (x)  
+# print(type(x))
+#Converting  str to int
+
+#print (type(y))
+#print (id(y))
+# y = int(x, 8)
+#print(y)
+
+# a = 123
+# b = float(a)
+# print(b)
+
+# b = complex(5,3.3)
+# print(b)
+# print(type(b))
+
+# Tuples to string
+# x = tuple('random')
+# print(x)
+
+# # Convert the string to a list
+# y = list('random')
+# print(y)
+
+# p = set('random')
+# print(p) 
+
+# print('this is a string')
+# print("i am a double string")
+# print("It's my birthday")
+# print('She said "I am hungry "')
+
+# test = 'random' 
+# print(test[-1])
+# #print(test[0:6:2])
+# print(test.index('d'))
+
+# tested = 'hello world Nigeria'
+# print(tested.split(" "))
+
+# print('first' + 'second')
+
+# print("it\'s my birthday")
+# #Tap space
+# print("it\'s \t my birthday")
+# #New line
+# print("it\'s my \n birthday")
+
+#List
+# a = [1,2,3,4,5,6]
+# print(a*3)
+
+# b = ['eggs', 'yam', 'banana', 1, 2, 3, 4, 5]
+# # print(b.index('yam'))
+# #b[0] = ['bread', 'mango', 'peckkkkkkky']
+# # b.extend([7,5,6,9])
+# # b.remove(9)
+
+# print(b.pop(0))
+
+#Tuples
+#t = (1,2,3,4,5)
+# print(t[2])
+
+#packing and Unpacking in Tuple and the elements shud be equal at both sides
+# t = (1,2,3,4,5)
+# (a,b,c,d,e) = t
+# print(d)
+
+#Dictionaries
+# d = { 'banana': "yellow",'eggs': "brown", 'yam': "white" }
+# d['eggs'] = 'orange'
+# del d['banana']
+
+# print(d.get('eggs'))
+# print(d.items())
+# print(d.keys())
+# print(d.popitem())
+
+# d2 = {'banana': "yellow",'banana': "green",}
+# print(d2)
+
+# Python Type Hirerachy
